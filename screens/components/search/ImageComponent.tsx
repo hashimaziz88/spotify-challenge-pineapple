2fa03d2bcef8d09aeac96dfe85907f19319b3c7e
Hashim Aziz Muhammad <hashim.muhammad@pineapple.co.za>
2025-09-10 14:22:04 +0200

    somehat happy with header image height and styling can be improved after other components complete

--- screens/components/search/ImageComponent.tsx
+++ screens/components/search/ImageComponent.tsx
@@ -1,14 +1,40 @@
 import React from "react";
-import { View, Image} from 'react-native';
+import { Image, StyleSheet, View, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const IMAGE_HEIGHT = 340;
 
 const ImageComponent: React.FC = () => {
   return (
-
-    <View>
-        <Image resizeMode="cover" source={require('../../../assets/images/header.jpg')}/>
+    <View style={styles.container}>
+      <Image
+        source={require('../../../assets/images/header.jpg')}
+        style={styles.image}
+        resizeMode="cover"
+      />
     </View>
-    );
-    };
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: width,
+    height: IMAGE_HEIGHT,
+    overflow: 'hidden',
+    backgroundColor: '#181818',
+    alignSelf: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  image: {
+    width: '100%',
+    height: '150%', // Make image taller than container
+    marginBottom: -IMAGE_HEIGHT * 0.18, // Shift image up so bottom is cut off
+  },
+});
     
 
 export default ImageComponent;
