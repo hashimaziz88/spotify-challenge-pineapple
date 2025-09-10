01998365b3fa6ee419ed8d6656131583d01bdf30
Hashim Aziz Muhammad <hashim.muhammad@pineapple.co.za>
2025-09-10 10:14:38 +0200

    android icons working not ios

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,13 @@
 // HomeScreen.tsx
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { MaterialDesignIcons } from '@react-native-vector-icons/material-design-icons';
 
 const HomeScreen: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <MaterialDesignIcons name="home" color="#ff0000" size={20} />
     </View>
   );
 };
